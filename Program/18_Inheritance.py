c2cefe436da9ee9d56f37d10b8150a05433fbaf3
victordev06 <victorjhernandez06@gmail.com>
2025-07-21 13:34:12 -0700

    Ubuntu_07_21_25

--- Program/18_Inheritance.py
+++ Program/18_Inheritance.py
@@ -66,4 +66,15 @@ x.printyear()
 """Add Methods"""
 # Add a Method called welcome to the Student class:
 
-#exercises
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+        
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+p1 = Student('Adrian','Hernandez', 2026)
+p1.welcome()
+
+# If you add a method in the child class with the same name, as a function in the parent class, the inheritance of the parent method will be overriden.
