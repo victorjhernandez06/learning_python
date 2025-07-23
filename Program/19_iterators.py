5f5b8d4b63579d30f98b6604ae6529619533d226
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-23 14:59:55 -0700

    Windows_07_23_25

---
+++ Program/19_iterators.py
@@ -0,0 +1,65 @@
+"""Python Iterators"""
+# an iterator is an object that contains a countable number of values.
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+# Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+
+"""Iterator vs Iterable"""
+# List, tuples, dicctionaries, and sets are iterable objects. They are iterable containers which you can get an iterator from.
+# All these objects have a iter() method which is used to get an iterator:
+
+# Example: Return an iterator from a tuple, and print each value:
+mytuple = ("apple","banana","cherry")
+myit = iter(mytuple)
+
+print(next(myit)) #--> apple
+print(next(myit)) #--> banana
+print(next(myit)) #--> cherry
+
+# Even strings are iterable objects, and can return an iterator
+# Example: Strings are also iterable objects, containing a sequence of characters:
+mystr = 'banana'
+myit = iter(mystr)
+
+print(next(myit)) #--> b
+print(next(myit)) #--> a
+print(next(myit)) #--> n
+print(next(myit)) #--> a
+print(next(myit)) #--> n
+print(next(myit)) #--> a
+
+"""Looping Through an iterator """
+# We can also use a for loop to iterate through an iterable object/
+# Example: iterate the values of a tuple:
+mystr1 = ('Apple')
+for x in mystr1:
+    print(x)
+
+# the for loop actually creates an iterator object and executes the next() method each loop.
+
+"""Create an Iterator"""
+# To create an object/class as an iterator you have to implement the methods __iter__() an __next__() to your Object.
+# As you have learned in the python classes/objects chapter, all classes have a function called __init__(), which allows you to do some initializing when the object is being created.
+# The __iter__() method acts similiar, you can do operations (initializing etc.) but must always return the iterator object itself.
+# The __next__() method also allows you to do operations, and must return the next item in the sequence.
+
+# Example: Create an iterator that returns numbers, starting with 1, and each sequence will increase by one( returning 1,2,3,4,5 etc.)
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter (myclass)
+
+print(next(myiter)) # --> 1
+print(next(myiter)) # --> 2
+print(next(myiter)) # --> 3
+print(next(myiter)) # --> 4
+print(next(myiter)) # --> 5
+
+
