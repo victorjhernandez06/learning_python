bc332f82ee5e2d9d957b7e709ac6d2d66ed03f95
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-23 15:29:51 -0700

    windows_07_23_25_pycharm

---
+++ Program/polymorphism.py
@@ -0,0 +1,17 @@
+"""Python Polymorphism"""
+# The word "Polymorphism" means "many forms", and in programming it refers ti methods/functions/operators with the same name that can be executed on many objects or class.
+
+"""Function Polymorphism"""
+# An example of a python function that can be used on different objects is the len() function.
+
+"""String"""
+# For strings len() returns the number of characters.
+# Example:
+x = "Hello World!"
+print(len(x)) #--> 12
+
+"""Tuple"""
+# For tuple len() returns the number of items in the tuple:
+mytuple = ('Computer','Laptop','Tablet')
+print(len(mytuple)) #--> 3
+
