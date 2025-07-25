9cfe92d45b4867b2dd789c400658b6fdb388b22f
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-25 00:18:18 -0700

    win_07_25_25_1

--- exercises-py/Python File Write.py
+++ exercises-py/Python File Write.py
@@ -1,4 +1,30 @@
 """Python File Write"""
+#
+# file = open('text.txt','a')
+# file.write('Test save on directories')
+# file.close()
+
+# file = open('text.txt','r')
+# print(file.read())
+
+# Encriptador
+def encriptar(texto):
+    # print('Funcion para Encriptar '+ texto)
+    textofinal =""
+    for letra in texto:
+        textofinal += letra + 'x'
+    return textofinal
+
+
+def desencriptar(texto):
+    # print('Funcion Desencriptar '+ texto)
+    textofinal = ""
+    contador = 0
+    for letra in texto:
+        if contador % 2 == 0:
+            textofinal += letra
+        contador += 1
+    return textofinal
 
 file = open('text.txt','a')
 file.write('Test save on directories')
