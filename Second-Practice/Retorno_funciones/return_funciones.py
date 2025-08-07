85c2124b69da2b3f5547fd731a867a2024283c38
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-08-07 16:01:20 -0700

    win

--- Second-Practice/Retorno_funciones/return_funciones.py
+++ Second-Practice/Retorno_funciones/return_funciones.py
@@ -4,7 +4,7 @@ def multiplicador(factor):
         return numero * factor
     return funcion_interna # Se retorna la funcion, pero coloca sin el parentesis para convertirla en funcion objeto.
 
-multiplicar_por_2 = multiplicador(2)
+multiplicar_por_2 = multiplicador(2) # Aqui indicamos el numero base, luego con la variable, vamos a decirle cual es el segundo numero.
 multiplicar_por_5 = multiplicador(5)
 
 print(multiplicar_por_2(3)) #yo aqui tengo que colocar un parametro, aqui siempre va a multiplicar por 2, el argumento que envie
