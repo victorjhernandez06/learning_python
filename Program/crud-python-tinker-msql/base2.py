94761da5affbb552a2cabe8ad4efaf18babcfb21
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-08-08 14:05:12 -0700

    windows_08_08_25

--- Program/crud-python-tinker-msql/base2.py
+++ Program/crud-python-tinker-msql/base2.py
@@ -8,8 +8,18 @@ from mysql.connector import Error #==> revisa la conexion remota.import
 # Debemos ejecutar ==> pip install mysql-connector-python
 
 
-
 ### --> FUNCIONES
+"""
+## En todas las interacciones de agregar, actualizar, buscar y borrar, antes del try:
+conexion = conectar()
+cursor = conexion.cursor()
+
+## Al finalizar el try:
+finally:
+        conexion.close()
+        
+#importamos de tk, ttk y messagebok de la libreria tkinter
+"""
 
 def conectar():
     try:
@@ -42,24 +52,62 @@ def insert():
 #siempre debemos cerrar la conexion a la base de datos. finally -> conexion.close()
 
 
-def editar():
+def actualizar():
     conexion = conectar()
     cursor = conexion.cursor()
-    sql ="UPDATE misdatos SET id=%s, nombre=%s, apellido=%s, telefono=%s, email=%s WHERE email=%s"
+    sql ="UPDATE misdatos SET nombre=%s, apellido=%s, telefono=%s, email=%s WHERE id=%s"
+    valores = (entry_id.get(), entry_nombre.get(), entry_apellido.get(), entry_telefono.get(), entry_email.get())
+    try:
+        cursor.execute(sql, valores)
+        conexion.commit()
+        messagebox.showinfo('Informacion',"Registro actualizado con exito!")
+        limpiar_campos()
+    except Error as e:
+        messagebox.showerror('Error',str(e))
+    finally:
+        conexion.close()
 
 def delete():
-    pass
+    conexion = conectar()
+    cursor = conexion.cursor()
+    sql = "DELETE FROM misdatos Where id=%s"
+    try:
+        cursor.execute(sql,(entry_id.get(),))
+        conexion.commit()
+        messagebox.showinfo('informacion','Registro eliminado con exito')
+        limpiar_campos()
+    except Error as e:
+        messagebox.showerror('Error',str(e))
+    finally:
+        conexion.close()
+
 
 def buscar():
-    pass
+    conexion = conectar()
+    cursor = conexion.cursor()
+    sql = "SELECT * FROM misdatos WHERE id=%s"
+    try:
+        cursor.execute(sql,(entry_id.get(),))
+        registro = cursor.fetchone()
+        if registro:
+            entry_nombre.insert(0, registro[1])
+            entry_apellido.insert(0, registro[2])
+            entry_telefono.insert(0, registro[3])
+            entry_email.insert(0, registro[4])
+        else:
+            messagebox.showinfo('Informacion', 'No se encontro el registro solicitado')
+    except Error as e:
+        messagebox.showerror('Error',str(e))
+    finally:
+        conexion.close()
 
-def clear():
-    pass
 
 def limpiar_campos():
-    pass
-
-#importamos de tk, ttk y messagebok de la libreria tkinter
+   entry_id.delete(0, tk.END)
+   entry_nombre.delete(0, tk.END)
+   entry_apellido.delete(0, tk.END)
+   entry_telefono.delete(0, tk.END)
+   entry_email.delete(0, tk.END)
 
 ### --> FORMULARIO.
 
@@ -89,11 +137,10 @@ entry_email=ttk.Entry(app)
 entry_email.grid(column=1, row=4)
 
 ttk.Button(app, text='insert', command=insert).grid(column=0, row=5)
-ttk.Button(app, text='editar', command=editar).grid(column=1, row=5)
+ttk.Button(app, text='actualizar', command=actualizar).grid(column=1, row=5)
 ttk.Button(app, text='eliminar', command=delete).grid(column=2, row=5)
 ttk.Button(app, text='buscar', command=buscar).grid(column=3, row=5)
-ttk.Button(app, text='limpiar', command=clear).grid(column=4, row=5)
-
+ttk.Button(app, text='limpiar', command=limpiar_campos).grid(column=4, row=5)
 
 
 app.mainloop()
