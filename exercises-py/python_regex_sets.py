aff85cebb1ef9fda0a7f915ebda9e84d3be4da01
victordev06 <victordev06@Victordev06.victordev06>
2025-08-07 18:06:07 -0700

    debian-08-07-25

--- exercises-py/python_regex_sets.py
+++ exercises-py/python_regex_sets.py
@@ -1,23 +1,68 @@
-"""[am]"""
+import re
+
+
+"""[arn]"""
 # Retuns a match where one of the specified characters (a,r,or n)is present
+txt = "The rain in Spain"
+#Check if the string has any a, r or n characters.
+x = re.findall('[arn]', txt)
+print(x)
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match!")
+    
+# ['r', 'a', 'n', 'n', 'a', 'n']
+# Yes, there is at least one match!
+print('-'*55)
+
 
 """[a-n]"""
+# Returns a match for any lower case character, alphabetically between a and n.
+
+txt = "The rain in Cojedes"
+x  = re.findall('[a-n]', txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match!")
+# ['h', 'e', 'a', 'i', 'n', 'i', 'n', 'j', 'e', 'd', 'e']
+# Yes, there is at least one match!
+print('-'*55)
 
 
 """[^arn]"""
+# Return a match for any character Except a, r, and n.
+
+txt = "the rain in Cojedes"
+x = re.findall('[^arn]', txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match!")
+# ['t', 'h', 'e', ' ', 'i', ' ', 'i', ' ', 'C', 'o', 'j', 'e', 'd', 'e', 's']
+# Yes, there is at least one match!
+print('-'*55)
+
 
 
 """[0123]"""
-
+# Return a match where any of the specified digits (0,1,2 or 3) are present
 
 """[0-9]"""
-
+# Returns a match for any digit between 0 and 9.
 
 """[0-5][0-9]"""
-
+# Returns a match for any two-digit numbers from 00 and 59
 
 
 """[a-z][A-Z]"""
-
+# Return a match for any character alphabetically between a and z, lower case OR upper case.
 
 """[+]"""
+
+# in sets +, * , . , | , (), $ , {}, has no special meaning, so [+] means, return a match for any + character in the string
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -1,6 +1,5 @@
 # This file must be used with "source bin/activate.csh" *from csh*.
 # You cannot run it directly.
-
 # Created by Davide Di Blasi <davidedb@gmail.com>.
 # Ported to Python 3.3 venv by Andrew Svetlov <andrew.svetlov@gmail.com>
 
@@ -9,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /media/victordev06/ba8f4057-c8db-4117-a632-746e776a8c12/home/victordev06/LearningPython/venv
+setenv VIRTUAL_ENV /home/victordev06/LearningPython/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
