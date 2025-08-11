8ec3c9b16250af20b27743727ebb4af4c39e9258
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-08-10 20:59:25 -0700

    windows_08_10_25'

--- exercises-py/python_regex_sets.py
+++ exercises-py/python_regex_sets.py
@@ -131,3 +131,117 @@ else:
   print("No match")
 # ['!', '!']
 # No match
+
+print('-'*55)
+
+"""THE FINDALL() FUCNTION"""
+# The findall() function returns a list containing matches
+
+# Example: Print a list of all matches
+txt = "the rain Spain"
+x =re.findall("ai", txt)
+print(x)
+# --> ['ai', 'ai']
+
+# The list contains the matches in the order they are found.
+# If no matches are found, an empty list is returned.
+
+
+print('-'*55)
+
+# Example: return an empty list if no match was found:
+
+x = re.findall("portugal", txt)
+print(x)
+
+# --> []
+
+""" THE SEARCH() FUNCTION """
+ # the search() function searches the string for a match, and returns a Match Object if there is a match.
+
+# if there is more than one match, only the first occurrence of the match will be returned:
+
+print('-'*55)
+
+# Example: Search for the first white-space character in the string:
+x = re.search("\s", txt)
+print("The first white-space character is located in position:", x.start())
+# --> The first white-space character is located in position: 3
+
+
+print('-'*55)
+
+#if no matches are found, the value None is returned:
+# Example: make a search thet returns no match:
+
+txt = "the rain in Spain"
+x = re.search("portugues", txt)
+print(x)
+# --> None
+
+
+print('-'*55)
+
+""" THE SPLIT() FUNCTION """
+# The split() function returns a list where the string has been split at each match:
+# Example: Split at each white-space charater
+
+x = re.split("\s", txt)
+print(x)
+# --> ['the', 'rain', 'in', 'Spain']
+
+print('-'*55)
+
+# You can control the number of occurrences by specifying the maxsplit parameter:
+#Example: split the string only at the first occurrence:
+x = re.split("\s", txt,1)
+print(x)
+
+# --> ['the', 'rain in Spain']
+print('-'*55)
+
+""" The Sub9) function """
+#the sub() function replaces the matcher with the text of your choice:
+# Example: replace every white-space character with the number 9:
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+# --> The9rain9in9Spain
+
+# Replace the first 2 occurrences:
+print('-'*55)
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+# --> The9rain9in Spain
+
+""" THE MATCH OBJECT """
+
+# A match Object is an object containing information about the search and the result.
+# Note: If there in no match, the valu None will be returned, instead of the Match Object.
+print('-'*55)
+
+#Example: Do o search that will return a Match Object:
+x = re.search("ai", txt)
+print(x) # this will print an object.
+
+# The Match object has properties and methods used to retrieve information about the search, and the result:
+
+## .span() Returns a tuple containing the start, and end positions of the match.
+## .string Returns the string passed into the function.
+## .group() Retuns the part of the string where there was a match.
+
+# Example: Print the position (start - and end position ) of the first match occurrence.
+# The regular expression looks for any words thet starts with an upper case "S":
+x = re.search(r"\bS|w+", txt)
+print(x.span()) #--> (12, 13)
+
+# Example: print the string passed into the function
+x =  re.search(r"\bS\w+",txt)
+print(x.group())
+# --> Spain
+
+
+
