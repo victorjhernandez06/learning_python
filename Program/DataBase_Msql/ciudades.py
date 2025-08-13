e7a96404d942c84b012afa8abea520d26435bf5b
victordev06 <victorjhernandez06@gmail.com>
2025-08-12 18:19:28 -0700

    Ubuntu_08_12_25

---
+++ Program/DataBase_Msql/ciudades.py
@@ -0,0 +1,49 @@
+from Cities import Cities
+
+
+# creamos un objeto
+cuidades = Cities()
+
+menu = """
+**************************
+*    MENU CIUDADES       *
+**************************
+*                        *
+* i) Insertar Ciudades   *
+* e) Eliminar Registro   *
+* m) Modificar Registros *
+* p) Imprimir la Tabla   *
+* x) Salir               *
+*                        *   
+**************************
+"""
+
+
+def main():
+    opcion = '0'
+    while opcion != 'X':
+        print(menu)
+        opcion = input("Que opcion deseas?").upper()
+        if opcion =='I':
+            print("**** insertar Paises ****")
+        elif  opcion =='E':
+            print("**** Eliminar Paises ****")
+        elif  opcion =='M':
+            print("*** Modificar Paises ***")
+        elif  opcion =='P':
+            print("*** Imprimir Paises ***")
+        elif  opcion =='X':
+            print("*** Saliendo ***")
+        else:
+            print("-> Invalid option")
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
