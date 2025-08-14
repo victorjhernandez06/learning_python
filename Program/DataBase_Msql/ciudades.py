2f2b1fa522254f7a26dbaf797e49a06fdfa9af2a
victordev06 <victorjhernandez06@gmail.com>
2025-08-14 00:21:25 -0700

    Ubuntu_08_14_25

--- Program/DataBase_Msql/ciudades.py
+++ Program/DataBase_Msql/ciudades.py
@@ -2,7 +2,7 @@ from Cities import Cities
 
 
 # creamos un objeto
-cuidades = Cities()
+ciudades = Cities()
 
 menu = """
 **************************
@@ -23,15 +23,31 @@ def main():
     opcion = '0'
     while opcion != 'X':
         print(menu)
-        opcion = input("Que opcion deseas?").upper()
+        opcion = input("Que opcion deseas?: ").upper()
         if opcion =='I':
             print("**** insertar Paises ****")
+            ISO3 = input("introduce la clave ISO3 del nuevo pais: ")
+            CountryName = input("introduce el nombre del nuevo pais: ")
+            Capital = input("introduce la capital del pais: ")
+            CurrencyCode = input("introduce el codigo de su moneda: ")
+            r = ciudades.insert_city(ISO3, CountryName, Capital, CurrencyCode)
+            if (r==0):
+                print("--> No se pudo insertar ningun pais")
+            else:
+                print("El pais se inserto correctamente")
         elif  opcion =='E':
             print("**** Eliminar Paises ****")
+            Id = int(input("introduce el Id del pais que desea eliminar: "))
+            r = ciudades.delete_city(Id)
+            if(r==0):
+                print("EL pais no existe")
+            else:
+                print("El pais se elimino correctamente")
         elif  opcion =='M':
             print("*** Modificar Paises ***")
         elif  opcion =='P':
             print("*** Imprimir Paises ***")
+            print(ciudades)
         elif  opcion =='X':
             print("*** Saliendo del sistema ***")
         else:
