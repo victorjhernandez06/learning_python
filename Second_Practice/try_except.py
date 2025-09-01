c9e11b07113b1b410ea6620c463ae9572b569268
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-08-31 22:52:25 -0700

    Windows_08_31_25

--- Second_Practice/try_except.py
+++ Second_Practice/try_except.py
@@ -39,3 +39,57 @@ except:
     print("Something went wrong")
 else:
     print("Nothing went wrong")
+
+"""FINALLY"""
+# The finally block, iff specified, will be executed regardless if the try block raises an error or not.
+try:
+    print(x)
+except:
+    print("something went wrong")
+finally:
+    print("the 'try except' is finished")
+
+# This can be useful to close objects and clean up resources:
+
+# Example: Try to open and write too a ile that is not writable
+try:
+    f = open("demofile.txt")
+    try:
+        f.write("Lorem ipsum")
+    except:
+        print("Something went wrong when writing to the file")
+    finally:
+        f.close()
+except:
+    print("Something went wrong when opening the file")
+
+# This can be useful to close objects and clean up resources:
+
+# Example: try to open and write to a file tha is not writable
+try:
+    f = open("demofile.txt")
+    try:
+        f.write("Lorem Ipsum")
+    except:
+        print("Something went wrogn when writing to the file")
+    finally:
+        f.close()
+except:
+    print("Something went wrong when opening the file_1")
+
+"""Raise an exception"""
+# As a Python developer you can choose tho throw an exception ig condition occurs.
+# To throw (or raise) an exception, use the raise keyword.
+#Example: Raise an error and stop the program if x is lower than 0:
+
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
+
+# The raise keyword is used to raise an exception.
+# You can define what kind of error to raise, and the text to print to the user.
+#Example: Raise a TypeWError if x is not an integer.
+x = "Hello"
+if not type(x) is int:
+    raise TypeError("Only integers are allowed")
