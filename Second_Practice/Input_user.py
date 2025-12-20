700d7f027883bee6e5d9f75c95f92f09b6a1b4d6
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-12-20 06:25:56 -0800

    Windows_12_20_25

---
+++ Second_Practice/Input_user.py
@@ -0,0 +1,5 @@
+"""Python None"""
+#None is a special constant in  Python that represents the absence of a value.
+# None es una constante especial en Python y representa la ausencia de un valor.
+
+#Its data type is NoneType, and None is the instance of a NoneType object.
