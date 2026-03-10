30ef6af9d7ccfa6fdc7cda32f65dffb323a56039
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2026-03-10 01:51:33 +0000

    09-03-26-II

--- Second_Practice/Strings/Strings.py
+++ Second_Practice/Strings/Strings.py
@@ -29,3 +29,31 @@ sin que se rompa el codigo'''
 print(a)
 print(b)
 
+#String are Arrays
+# like many other popular programming languages, strings in python are arrays of bytes representing unicode characters
+# However, python does not have a character data type, a single character is simply a string with a length of 1
+# Square brackets can be used to access elements of the string
+
+c = "Hello Victor"
+print(c[1])
+
+#Looping through a String
+# Since strings are arrays, we can loop through the characters in a string, with a for loop
+for x in "Hernandez":
+    print(x)
+    
+
+#String Length
+# To get the length of a string, use the built-in len() function
+# Para obtener la longitud de una cadena, use la función incorporada len()
+d = "Hello karina"
+print(len(d))
+
+#Check String
+# To check if a certain phrase or character is present in a string, we can use the keyword in
+# Para verificar si una cierta frase o carácter está presente en una cadena, podemos usar la palabra clave in
+txt = "The best things in life are free!"
+print("free" in txt)
+print("best" in txt)
+
+
