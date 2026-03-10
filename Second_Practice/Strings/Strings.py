6999f56716404d670ddd08ef31a4f4bf2ffc6ddf
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2026-03-10 00:54:39 +0000

    9-3-2026

---
+++ Second_Practice/Strings/Strings.py
@@ -0,0 +1,31 @@
+# Python Strings
+#Strings in python are sourronded by either single quotation marks, or double quotation marks
+# "Hello" is the same as 'Hello'
+# We can display a string literal with the print() function
+print("Hello")
+print('Hello')
+
+#Quotes inside Quotes
+#Yoy can use quotes inside a string as long as they don't match the quotes aurrounding the string the string
+
+print("It's a nice day")
+print('She said "Hello"')
+print('He is called "Victor"')
+
+#Assign String to a Variable
+a = "Hello Victor"
+print(a)
+
+#Multiline Strings
+#You can assign a multiline string to a variable by using three quotes
+a = """ Aqui puedo escribir un texto,
+dar enter y poder escribir en la linea siguiente
+sin que se rompa el codigo"""
+
+# Or Three single Quotes
+b = ''' Aqui puedo escribir un texto,
+dar enter y poder escribir en la linea siguiente
+sin que se rompa el codigo'''
+print(a)
+print(b)
+
