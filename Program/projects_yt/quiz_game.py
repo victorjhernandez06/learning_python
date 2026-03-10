708c29e98bb38c3e535bb888fed1614388a043a9
victorjhernandez06 <victorjhernandez06.com>
2026-03-09 21:32:08 -0700

    Projects March

--- Program/projects_yt/quiz_game.py
+++ Program/projects_yt/quiz_game.py
@@ -1,12 +1,17 @@
 print("Welcome to the Quiz Game!")
 playing =  input(("Do you want to play?"))
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
-print("Okay! Let's play :)")
+print("Okay!, Let's play! :)")
+
 answer = input("What does CPU stand for? ")
-if answer == "Central Processing Unit":
-    print("Correct!")
+if answer == "central processing unit":
+    print('Correct!')
 else:
-    print("Incorrect! The correct answer is Central Processing Unit.")    
+    print('Incorrect!')
+
+answer = input("What does GPU stand for? ")
+
+
